8d399a6e908fc449b4be3a1f82938964ea88ca45
WHK <weehekun@gmail.com>
2018-05-25 11:02:12 +0800

    Fixed direction
    It doesn't exactly matter but still

--- accelerometer.py
+++ accelerometer.py
@@ -2,9 +2,9 @@ from microbit import *
 while True:
     reading = accelerometer.get_x()
     """ while you're at it, try get_y() and get_z() ;) """
-    if reading > 20:
-        display.show("L")
-    elif reading < -20:
+    if reading > 40:
         display.show("R")
+    elif reading < -40:
+        display.show("L")
     else:
         display.show("-")
